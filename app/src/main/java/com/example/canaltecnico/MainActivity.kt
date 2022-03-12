2d36a0e809c079dee292f14e445a28d1c2b84bc0
Jordan Claudino <jordanclaudino@gmail.com>
2022-03-12 00:57:38 -0300

    First commit with layout configurations

---
+++ app/src/main/java/com/example/canaltecnico/MainActivity.kt
@@ -0,0 +1,22 @@
+package com.example.canaltecnico
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.canaltecnico.databinding.ActivityMainBinding
+
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
+    }
+
+
+}
